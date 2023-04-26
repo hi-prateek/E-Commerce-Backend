d7e72ffe7819f40d71a7036dd7e6c8b4e37c43fd
Prateek Pandey <48723556+prateek2575@users.noreply.github.com>
2023-04-26 15:22:08 +0530

    Update product.js

--- projbackend/models/product.js
+++ projbackend/models/product.js
@@ -8,43 +8,54 @@ const productSchema = new mongoose.Schema({
         maxlength:32,
         trim:true,
         required:true
-
     },
     description: {
         type:String,
         maxlength:2000,
         required:true,
         trimtrue
-
     },
     price: {
         type:Number,
         maxlength: 32,
         required: true,
         trim: true
-
     },
     category:{
         type: ObjectID,
         ref: "Category",
         required:true
-
+    },
+    brand: {
+        type:String,
+        maxlength: 32,
+        trim:true
+    },
+    color: {
+        type:String,
+        maxlength: 32,
+        trim:true
+    },
+    size: {
+        type:String,
+        maxlength: 32,
+        trim:true
+    },
+    shipping: {
+        type:Boolean,
+        required:true
     },
     stock:{
         type: Number,
     },
-
     soldUnits:{
         type: Number,
         default:0,
-        },
-
+    },
     photo:{
         data: Buffer,
         contentType: String
-        
     },
-
 }, {timestamps: true});
 
 module.exports = mongoose.model("Product", productSchema);
